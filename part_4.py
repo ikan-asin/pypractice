957f621e3b708331bdbed8fa595208885de94d52
ikanasin <sherlyhu28@gmail.com>
2021-02-14 18:47:19 +0700

    fix part 4

--- part_4.py
+++ part_4.py
@@ -1,12 +1,30 @@
-class cool:
-	def __init__(self,*args):
-		self.args = args
+import random
 
-	def plus(self):
-		a = 0
-		for i in self.args:
-			a = a + i
-		print(a)
+class game:
+	def __init__(self,name):
+		self.name = name
+		self.exp = 0
+		self.level = 1
 
-data = cool(2,10,-50,-60)
-data.plus()
+	def info(self):
+		print('name :', self.name)
+		print('exp :',self.exp)
+		print('level :',self.level,'\n')
+
+	def attack(self):
+		self.exps = random.randint(20,30)
+	
+	def collect_exp(self):
+		game.attack(self)
+		
+		self.exp += self.exps
+		if self.exp > 200:
+			print(self.name, 'level up !')
+			self.level += 1
+			self.exp -= 200
+
+data = game('ikan')
+
+for i in range(10):
+	data.collect_exp()
+	data.info()
